b6400dc66236fbd10a52cee5df2c11de5e00b86c
David Song <songdasol@gmail.com>
2022-02-24 20:36:11 -0800

    completed js loops practice.

---
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -0,0 +1,7 @@
+/* exported addSuffixToAll */
+function addSuffixToAll(words, suffix) {
+  var suffixAdded = [];
+  for (var i = 0; i < words.length; i++) {
+    suffixAdded.push(words[i] + suffix);
+  } return suffixAdded;
+}
