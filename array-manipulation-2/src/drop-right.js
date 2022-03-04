97bd90779ca367c76740854bae95e264931e2981
David Song <songdasol@gmail.com>
2022-03-03 19:11:03 -0800

    completed array mani 2.

---
+++ array-manipulation-2/src/drop-right.js
@@ -0,0 +1,10 @@
+/* exported dropRight */
+function dropRight(array, count) {
+  return array.slice(0, -count);
+}
+
+/*
+1. Define a function with 2 parameters
+2. Use the slice method to start pushing from the first item to the count backwards
+3. Return the value of array
+*/
---
+++ array-manipulation-2/src/take-right.js
@@ -0,0 +1,10 @@
+/* exported takeRight */
+function takeRight(array, count) {
+  return array.slice(-count, array.length);
+}
+
+/*
+1. Define a function with 2 parameters
+2. Use the slice method to count backwards from the start and end with the length of the array
+3. Return the value of array
+*/
