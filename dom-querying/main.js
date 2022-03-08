873137adb7845a73a0b8805a2e1547124e387fcb
David Song <songdasol@gmail.com>
2022-03-07 19:02:28 -0800

    completed dom querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,20 @@
+var hello = 'hello, world';
+console.log(hello);
+
+var $heading = document.querySelector('h1');
+console.log($heading);
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+console.log($explanation);
+console.dir($explanation);
+
+var $hint = document.querySelector('p.hint');
+console.log($hint);
+console.dir($hint);
+
+var $pElements = document.querySelectorAll('p');
+console.log($pElements);
+
+var $examples = document.querySelectorAll('.example-link');
+console.log($examples);
